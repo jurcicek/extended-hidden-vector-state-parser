8fa5950808f78010ee49deb6170aa623baa2a6ed
honza.svec <honza.svec@3a267be6-d04a-0410-a0df-2b75c7826220>
2008-06-17 12:56:11 +0000

    Sample result of test run is included (see results/sample-run)

---
+++ trunk/semantics-4/results/sample-run/include/dataConstants.h
@@ -0,0 +1,20 @@
+#define CONCEPT_CARD   35
+
+% I pruned 32.7% symbols (lemma) from the original vocabulary.
+#define S1_CARD   265
+
+% I pruned 53.3% symbols (pos) from the original vocabulary.
+#define S2_CARD   83
+
+% I pruned 0.0% symbols (off) from the original vocabulary.
+#define S3_CARD   5
+
+
+% the cardinality should be CONCEPT_CARD^DEPTH_OF_STACK, but I know that the stack values are sparse
+#define JOINT_C1C2C3C4_CARD     207
+
+% the cardinality should be CONCEPT_CARD^DEPTH_OF_STACK, but I know that the stack values are sparse
+#define JOINT_C1C2C3C4_CARD     207
+
+% the cardinality should be CONCEPT_CARD^DEPTH_OF_STACK, but I know that the stack values are sparse
+#define JOINT_C1C2C3C4_CARD     207
