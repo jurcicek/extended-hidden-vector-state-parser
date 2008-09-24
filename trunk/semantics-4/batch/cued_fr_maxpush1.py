addae32618298d5bd3ea3d1b698f37dc8fdbaa39
filip.jurcicek <filip.jurcicek@3a267be6-d04a-0410-a0df-2b75c7826220>
2008-09-24 14:46:05 +0000

    renaming batches

---
+++ trunk/semantics-4/batch/cued_fr_maxpush1.py
@@ -0,0 +1,14 @@
+from svc.utils import linrange, linspace
+
+#settings['DATA_REDUCTION']=10
+
+prepareData(env=env)
+train(env=env)
+forcealignTrn(env=env)
+smooth(env=env)
+scale(env=env)
+print decodeHldt()
+print decodeTst()
+
+#fsmconvert()
+moveResults(True)
