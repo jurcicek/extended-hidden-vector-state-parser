543f42f6fed1719107aea3175c17eb3b8d1ac327
filip.jurcicek <filip.jurcicek@3a267be6-d04a-0410-a0df-2b75c7826220>
2008-09-24 14:46:05 +0000

    renaming batches

---
+++ semantics-4/batch/cued_fr_no_scaling.py
@@ -0,0 +1,16 @@
+from svc.utils import linrange, linspace
+
+settings['DATA_REDUCTION']=10
+settings['SCALE_CONCEPT12']=1.0
+settings['SCALE_PUSHPOP']=1.0
+
+prepareData(env=env)
+train(env=env)
+forcealignTrn(env=env)
+smooth(env=env)
+scale(env=env)
+print decodeHldt()
+print decodeTst()
+
+#fsmconvert()
+moveResults(True)
